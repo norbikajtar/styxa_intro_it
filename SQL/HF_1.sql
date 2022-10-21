ddffc7dfeb34f310ab3394a76b4781c7c08aea35
norbikajtar <norbikajtar@gmail.com>
2022-10-21 11:53:49 +0300

    added last three examples of SQL scripts

--- SQL/HF_1.sql
+++ SQL/HF_1.sql
@@ -9,8 +9,7 @@ SELECT Students.Name, Students.Surname,Students.Date_of_Birth, Students.Group_Id
   SELECT Students.Name, Students.Surname, Students.Group_Id
   FROM Students INNER JOIN Marks ON (Students.Stud_Id=Marks.Student_Id)
   INNER JOIN Teachers ON (Marks.Teacher_Id=Teachers.Teacher_Id)
-  WHERE Teachers.Name='Adrian'
-  ;
+  WHERE Teachers.Name='Adrian';
 
 
   SELECT s1.name ,m1.Mark, s1.surname, s1.Group_Id FROM 
@@ -21,4 +20,23 @@ SELECT Students.Name, Students.Surname,Students.Date_of_Birth, Students.Group_Id
   JOIN Disciplines AS d2 ON 
   (d2.Discipline_Id= m2.Disc_Id)
   WHERE Name ='Barbara' AND d2.Disc_name='History')
+
+  SELECT TOP 3 s.Name, s.Surname, s.Group_Id
+  FROM Students s
+  JOIN Marks m ON m.Student_Id = s.Stud_Id
+  JOIN Disciplines d ON m.Disc_Id = d.Discipline_Id
+  WHERE d.Disc_name = 'Computer Programming'
+  ORDER BY Mark
+
+  SELECT s.Name, s.Surname, AVG(m.Mark) AverageMark
+  FROM Students s
+  JOIN Marks m ON m.Student_Id = s.Stud_Id
+  JOIN Groups g ON s.Group_Id = g.Gr_Id
+  WHERE g.Gr_name = 'vestibulum'
+  Group By s.Name, s.Surname
+
+  SELECT g.Gr_name, g.Comments, COUNT(s.Stud_Id) StudentCount
+  FROM Groups g
+  JOIN Students s ON s.Group_Id = g.Gr_Id
+  Group By g.Gr_name, g.Comments
 
